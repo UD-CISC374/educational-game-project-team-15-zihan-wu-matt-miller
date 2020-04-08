f465643d3f9d7b93f09f34fe07b1612f8767b91d
Matt <matt.miller041798@gmail.com>
2020-04-08 15:21:15 -0400

    Updates

---
+++ precache-manifest.b6cd39d1498f728648d95cac37d7ba00.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7087d5d91321d575b038366ef9f2407",
+    "url": "assets/citations.md"
+  },
+  {
+    "revision": "53cefca2a0f75654486b180949686165",
+    "url": "assets/icons/favicon.ico"
+  },
+  {
+    "revision": "8ab30def0ad109a97117bd62db942d75",
+    "url": "assets/icons/icons-16.png"
+  },
+  {
+    "revision": "feee71db2bb3ba5d11b26658baf97783",
+    "url": "assets/icons/icons-192.png"
+  },
+  {
+    "revision": "cb71713be71d0b8820c7ea6af9c5ddc6",
+    "url": "assets/icons/icons-512.png"
+  },
+  {
+    "revision": "200ce541edfaa71be93a1041736d1fee",
+    "url": "assets/icons/icons8-game-controller-16.png"
+  },
+  {
+    "revision": "53cefca2a0f75654486b180949686165",
+    "url": "favicon.ico"
+  },
+  {
+    "revision": "1558f280aeef4f6f153e6537edb420eb",
+    "url": "index.html"
+  },
+  {
+    "revision": "2d7aff72c7980729fd2c",
+    "url": "main.3d1c882cba482391e26e.bundle.js"
+  },
+  {
+    "revision": "003187cab0ecf7a20e343e2ba6db4a34",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "a8029749c7edd38204cf67287cb14139",
+    "url": "sw.js"
+  },
+  {
+    "revision": "5e69403aa42e950fedff",
+    "url": "vendors.0805bb80903235d40eaf.bundle.js"
+  }
+]);
---
+++ sw.js
@@ -0,0 +1,11 @@
+importScripts("precache-manifest.b6cd39d1498f728648d95cac37d7ba00.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+/**
+ * You should only modify this, if you know what you are doing.
+ * This phaser template is using workbox (https://developers.google.com/web/tools/workbox/)
+ * to precache all assets.
+ * It uses the InjectManifest function from 'workbox-webpack-plugin' inside
+ * webpack/webpack.common.js
+ */
+workbox.precaching.precacheAndRoute(__precacheManifest)
+
