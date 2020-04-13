90939e2432b94826ee9479409690ed566e40a64e
Anna Wu <annapoliswu@gmail.com>
2020-04-12 23:43:13 -0400

    react on overlap, currently player changes color on certain tiles. added var types

--- src/scripts/scenes/mainScene.ts
+++ src/scripts/scenes/mainScene.ts
@@ -4,9 +4,12 @@ import Player from '../objects/player';
 export default class MainScene extends Phaser.Scene {
   private exampleObject: ExampleObject;
   player:Player;
-  cursorKeys;
-  playerXVelocity = 100;
-  playerYVelocity = 100;
+  cursorKeys: Phaser.Types.Input.Keyboard.CursorKeys;
+  map: Phaser.Tilemaps.Tilemap;
+  tileset: Phaser.Tilemaps.Tileset;
+  belowLayer:Phaser.Tilemaps.StaticTilemapLayer;
+  worldLayer:Phaser.Tilemaps.StaticTilemapLayer;
+  aboveLayer:Phaser.Tilemaps.StaticTilemapLayer;
 
   constructor() {
     super({ key: 'MainScene' });
@@ -14,35 +17,48 @@ export default class MainScene extends Phaser.Scene {
 
   create() {
     this.cursorKeys = this.input.keyboard.createCursorKeys();
-
-    // When loading from an array, make sure to specify the tileWidth and tileHeight
-    const map = this.make.tilemap({ key: "map" });
-    const tileset = map.addTilesetImage("pkmn", "tiles");
+    this.map = this.make.tilemap({ key: "map" });
+    this.tileset = this.map.addTilesetImage("pkmn", "tiles");
     
     // Parameters: layer name (or index) from Tiled, tileset, x, y
-    const belowLayer = map.createStaticLayer("Below Player", tileset, 0, 0);
-    const worldLayer = map.createStaticLayer("World", tileset, 0, 0);
-    this.player = new Player(this, 20, 20);
-    const aboveLayer = map.createStaticLayer("Above Player", tileset, 0, 0);
-    worldLayer.setCollisionByProperty({ collides: true });
+    this.belowLayer = this.map.createStaticLayer("Below Player", this.tileset, 0, 0).setDepth(-1);
+    this.worldLayer = this.map.createStaticLayer("World", this.tileset, 0, 0);
+    this.aboveLayer = this.map.createStaticLayer("Above Player", this.tileset, 0, 0).setDepth(1);
+    this.worldLayer.setCollisionByProperty({ collides: true });
 
     const debugGraphics = this.add.graphics().setAlpha(0.75);
-    worldLayer.renderDebug(debugGraphics, {
+    this.worldLayer.renderDebug(debugGraphics, {
       tileColor: null, // Color of non-colliding tiles
       collidingTileColor: new Phaser.Display.Color(243, 134, 48, 255), // Color of colliding tiles
       faceColor: new Phaser.Display.Color(40, 39, 37, 255) // Color of colliding face edges
     });
 
+    this.player = new Player(this, 20, 20);
+    this.physics.add.collider(this.player, this.worldLayer); 
+    this.physics.add.collider(this.player, this.belowLayer); // add collider but don't set collision for overlap callback
+    this.player.tint = 0xff00ff;
 
-    this.physics.add.collider(this.player, worldLayer);
-    this.player.tint = 0x0000ff;
+    //not reading set collision points from json file?
+    this.belowLayer.setTileIndexCallback([30,31,32,33], ()=>{
+      this.player.tint = 0xff0000;
+    }, this);
 
-    //belowLayer.setCollisionByProperty({color:"red"});
-    //this.physics.add.overlap(this.player, aboveLayer, this.changeColor, undefined, this);
+    this.belowLayer.setTileIndexCallback([125,126,150,151], ()=>{
+      this.player.tint = 0xff00ff;
+    }, this);
+
+    //testing stuff, ignore this
+    this.belowLayer.tilemap.layer.data.forEach( i =>{
+      i.forEach(j =>{
+        if(j.index == 32){
+
+        }
+      })
+    });
   }
   
   changeColor(){
-    this.player.tint = 0x0000ff;
+    this.player.tint = 0x00ffff;
     console.log("ehhhhhh");  
   }
 
