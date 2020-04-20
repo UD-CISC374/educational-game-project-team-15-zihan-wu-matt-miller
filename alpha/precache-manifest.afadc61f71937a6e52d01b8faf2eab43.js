b586909d54fcc6ee24e1b6a7fec5970e166dcb89
Anna Wu <annapoliswu@gmail.com>
2020-04-20 00:27:56 -0400

    Updates

---
+++ alpha/precache-manifest.afadc61f71937a6e52d01b8faf2eab43.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c367097bf3106f2fdf01aa4b29c6fcb0",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/alphaassets/camocaper.json"
+  },
+  {
+    "revision": "e3ded070cc7059e41d2a48a52bfd993a",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/alphaassets/camocaper.tmx"
+  },
+  {
+    "revision": "cd016ddf1954a11d06ddcd8f1ae6e616",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/alphaassets/camotiles.png"
+  },
+  {
+    "revision": "fba88d26c7328028e97d39f9ad947696",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/alphaassets/catchara.png"
+  },
+  {
+    "revision": "5250d91a60f768b52fb79ab2591efcaa",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/alphaassets/citations.md"
+  },
+  {
+    "revision": "53cefca2a0f75654486b180949686165",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/alphaassets/icons/favicon.ico"
+  },
+  {
+    "revision": "8ab30def0ad109a97117bd62db942d75",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/alphaassets/icons/icons-16.png"
+  },
+  {
+    "revision": "feee71db2bb3ba5d11b26658baf97783",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/alphaassets/icons/icons-192.png"
+  },
+  {
+    "revision": "cb71713be71d0b8820c7ea6af9c5ddc6",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/alphaassets/icons/icons-512.png"
+  },
+  {
+    "revision": "200ce541edfaa71be93a1041736d1fee",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/alphaassets/icons/icons8-game-controller-16.png"
+  },
+  {
+    "revision": "2f664528ea817529fe8fd6f320419725",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/alphaassets/tutorial/pkmn.png"
+  },
+  {
+    "revision": "f3a8b37f3ded924719d6a3acdb5d1a5e",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/alphaassets/tutorial/tiledemo.json"
+  },
+  {
+    "revision": "53cefca2a0f75654486b180949686165",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/alphafavicon.ico"
+  },
+  {
+    "revision": "351383963360f206940e",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/alphagen.main.a9b5ce63da39a7385069.bundle.js"
+  },
+  {
+    "revision": "ed4e65c0ef1d262f0ba2",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/alphagen.vendors.24a045468bbd44191099.bundle.js"
+  },
+  {
+    "revision": "c9cfdf73d250c98fa823f91c4f60308e",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/alphaindex.html"
+  },
+  {
+    "revision": "e3a72ed08fea663494fd70b7b7e9beff",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/alphamanifest.json"
+  },
+  {
+    "revision": "a8029749c7edd38204cf67287cb14139",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/alphasw.js"
+  }
+]);
