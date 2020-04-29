19f05f925702374df4a23962db7e8476f309755c
Matt <matt.miller041798@gmail.com>
2020-04-29 19:56:05 -0400

    changed button color and added another button

--- src/scripts/scenes/startScene.ts
+++ src/scripts/scenes/startScene.ts
@@ -5,6 +5,7 @@ export default class StartScene extends Phaser.Scene{
 
     background: Phaser.GameObjects.Image;
     play_button: Phaser.GameObjects.Image;
+    controls_button: Phaser.GameObjects.Image;
 
     constructor(){
         super({key: 'StartScene'});
@@ -19,8 +20,14 @@ export default class StartScene extends Phaser.Scene{
 
     preload(){
         this.load.image("start-bkg","assets/start-bkg-reg.jpg");
-        this.load.image("play-bttn-dwn","assets/play-bttn-dwn.png");
-        this.load.image("play-bttn-up","assets/play-bttn-up.png");
+        //this.load.image("play-bttn-dwn","assets/play-bttn-dwn.png");
+        //this.load.image("play-bttn-up","assets/play-bttn-up.png");
+
+        this.load.image("play-bttn-dwn","assets/play-3-dwn.png");
+        this.load.image("play-bttn-up","assets/play-3-up.png");
+
+        this.load.image("ctrl-bttn-dwn","assets/controls-3-dwn.png");
+        this.load.image("ctrl-bttn-up","assets/controls-3-up.png");
     }
 
     create(){
@@ -33,16 +40,36 @@ export default class StartScene extends Phaser.Scene{
         this.play_button.on('pointerover',(event) => {
             this.play_button.setTexture('play-bttn-dwn');
             this.play_button.setScale(1.1);
+            // play sound???
         });
         this.play_button.on('pointerout',(event) => {
             this.play_button.setTexture('play-bttn-up');
             this.play_button.setScale(1);
+            // play sound???
         });
         this.play_button.on('pointerup',(event) => {
             this.scene.start('MainScene');
+            // Play clicking sound???
         });
 
-        //this.background.scaleX = 0.8;
+        this.controls_button = this.add.image(this.centerX, this.centerY + (this.play_button.height * 1.5), 'ctrl-bttn-up');
+        this.controls_button.setInteractive();
+
+        // Set those interactions
+        this.controls_button.on('pointerover',(event) => {
+            this.controls_button.setTexture('ctrl-bttn-dwn');
+            this.controls_button.setScale(1.1);
+            // play sound???
+        });
+        this.controls_button.on('pointerout',(event) => {
+            this.controls_button.setTexture('ctrl-bttn-up');
+            this.controls_button.setScale(1);
+            // play sound???
+        });
+        this.controls_button.on('pointerup',(event) => {
+            // Jump to another scene...
+            // Play clicking sound???
+        });
     }
 
     update(){
