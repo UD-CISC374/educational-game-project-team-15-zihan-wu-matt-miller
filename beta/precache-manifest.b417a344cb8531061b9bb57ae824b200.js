05aa7fc1499d28211865bfdd6e81c315cbf5e2ed
Anna Wu <annapoliswu@gmail.com>
2020-05-03 18:34:51 -0400

    Updates

---
+++ beta/precache-manifest.b417a344cb8531061b9bb57ae824b200.js
@@ -0,0 +1,214 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "369c0235a668e6e3ed40ed16319728d0",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/betaassets/arrow-keys-full.png"
+  },
+  {
+    "revision": "e00b9f7c33fef8b9b343155cabdabc00",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/betaassets/arrow-keys-single.png"
+  },
+  {
+    "revision": "521882646056092e2a90cee445c49d1f",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/betaassets/back-down.png"
+  },
+  {
+    "revision": "4250a9fa43ecce767ca6829eee466745",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/betaassets/back-up.png"
+  },
+  {
+    "revision": "213944f46da4fe1dd10b2b6c40f444f7",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/betaassets/bkg.png"
+  },
+  {
+    "revision": "0f3ced6e78933083c5d459fb3e3239ec",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/betaassets/bluegem.png"
+  },
+  {
+    "revision": "4e3bba8bd63519433341cf53fa9a6c9a",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/betaassets/button-template.png"
+  },
+  {
+    "revision": "ada77b74fa425cbf2679a8e51c49f2fa",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/betaassets/camocaper.json"
+  },
+  {
+    "revision": "e3ded070cc7059e41d2a48a52bfd993a",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/betaassets/camocaper.tmx"
+  },
+  {
+    "revision": "cd016ddf1954a11d06ddcd8f1ae6e616",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/betaassets/camotiles.png"
+  },
+  {
+    "revision": "fba88d26c7328028e97d39f9ad947696",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/betaassets/catchara.png"
+  },
+  {
+    "revision": "800096c055837f103d79dd0d0039224e",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/betaassets/citations.md"
+  },
+  {
+    "revision": "6757b9c8356c820347802d04419631e6",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/betaassets/controls-3-dwn.png"
+  },
+  {
+    "revision": "a64714df2e0287e050a301fff539dd3e",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/betaassets/controls-3-up.png"
+  },
+  {
+    "revision": "d8976d308346afdcd24a9de8c4d6b5fb",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/betaassets/gem.png"
+  },
+  {
+    "revision": "53cefca2a0f75654486b180949686165",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/betaassets/icons/favicon.ico"
+  },
+  {
+    "revision": "8ab30def0ad109a97117bd62db942d75",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/betaassets/icons/icons-16.png"
+  },
+  {
+    "revision": "feee71db2bb3ba5d11b26658baf97783",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/betaassets/icons/icons-192.png"
+  },
+  {
+    "revision": "cb71713be71d0b8820c7ea6af9c5ddc6",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/betaassets/icons/icons-512.png"
+  },
+  {
+    "revision": "200ce541edfaa71be93a1041736d1fee",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/betaassets/icons/icons8-game-controller-16.png"
+  },
+  {
+    "revision": "4cea6c8e0454de7a59c3c2acd5a3372a",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/betaassets/info-formatted.png"
+  },
+  {
+    "revision": "e2211c4f1f95c441cc283dfd117c6492",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/betaassets/info.png"
+  },
+  {
+    "revision": "94ecadd667fc2f6a0e56d54a0108a286",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/betaassets/jail.png"
+  },
+  {
+    "revision": "fe2a93637af3b9c72b0069ef74f069c5",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/betaassets/keys.png"
+  },
+  {
+    "revision": "2c7850f2bca7beec7e91e9c615774f09",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/betaassets/level-1.json"
+  },
+  {
+    "revision": "21048413a6e69b2202b027ea7644870a",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/betaassets/level-2.json"
+  },
+  {
+    "revision": "08e98aef08f724ccb7784da614369ba8",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/betaassets/level-3.json"
+  },
+  {
+    "revision": "05001735150d9f7d214701ea2df32f9a",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/betaassets/move-tutorial-black-1.png"
+  },
+  {
+    "revision": "bb37625dea896f7814a27841d6fa6df6",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/betaassets/move-tutorial-black.png"
+  },
+  {
+    "revision": "6b38f81cf61c5bc1e76c32591ecd3e29",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/betaassets/move-tutorial-white-1.png"
+  },
+  {
+    "revision": "511b965fcdc74918498051c39575f846",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/betaassets/move-tutorial-white.png"
+  },
+  {
+    "revision": "e4266d1f808e8a241b4eaf2a05190006",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/betaassets/play-2-black.png"
+  },
+  {
+    "revision": "fcb1418f37af597f01cd7df1aaa044a5",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/betaassets/play-2-white.png"
+  },
+  {
+    "revision": "78d9e79bdf7caeadf38fb8a5a07b4111",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/betaassets/play-2.png"
+  },
+  {
+    "revision": "3d64f149523e2f7b7a58d7532d359d68",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/betaassets/play-3-black.png"
+  },
+  {
+    "revision": "26915c601dd62925b8dd60961df8e350",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/betaassets/play-3-dwn.png"
+  },
+  {
+    "revision": "fbc44333ed4dfc08898b6c672bb25a0b",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/betaassets/play-3-up.png"
+  },
+  {
+    "revision": "8077e70ea86f853b2e43c344ca85068d",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/betaassets/play-3-white.png"
+  },
+  {
+    "revision": "0a550269321c170cdbfd68c98647fce5",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/betaassets/play-3.png"
+  },
+  {
+    "revision": "17910e90e56f864e7366a3efed02145b",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/betaassets/play-bttn-dwn.png"
+  },
+  {
+    "revision": "5d82c81ce22e5de22db4cfad860709fe",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/betaassets/play-bttn-up.png"
+  },
+  {
+    "revision": "885d705734700dbe57579e0135c46747",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/betaassets/start-bkg-4k.jpg"
+  },
+  {
+    "revision": "7c9aa037a67baff060c99e3733da3b49",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/betaassets/start-bkg-reg.jpg"
+  },
+  {
+    "revision": "00e552c176c7beeb813933fc9f5b3d3e",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/betaassets/start-bkg-reg.png"
+  },
+  {
+    "revision": "291ad4b402061b8d26947a95528b9151",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/betaassets/title.png"
+  },
+  {
+    "revision": "2f664528ea817529fe8fd6f320419725",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/betaassets/tutorial/pkmn.png"
+  },
+  {
+    "revision": "f3a8b37f3ded924719d6a3acdb5d1a5e",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/betaassets/tutorial/tiledemo.json"
+  },
+  {
+    "revision": "53cefca2a0f75654486b180949686165",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/betafavicon.ico"
+  },
+  {
+    "revision": "2d144fb0b9128198b064",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/betagen.main.f8cedbf6713986ef2418.bundle.js"
+  },
+  {
+    "revision": "a5db9487b03595b04940",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/betagen.vendors.7a966c368cb1a1bd41f1.bundle.js"
+  },
+  {
+    "revision": "340e0b054e4c8bfa6aaba6746c3a8b9f",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/betaindex.html"
+  },
+  {
+    "revision": "e3a72ed08fea663494fd70b7b7e9beff",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/betamanifest.json"
+  },
+  {
+    "revision": "a8029749c7edd38204cf67287cb14139",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/betasw.js"
+  }
+]);
---
+++ beta/sw.js
@@ -0,0 +1,11 @@
+importScripts("/educational-game-project-team-15-zihan-wu-matt-miller/betaprecache-manifest.b417a344cb8531061b9bb57ae824b200.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+/**
+ * You should only modify this, if you know what you are doing.
+ * This phaser template is using workbox (https://developers.google.com/web/tools/workbox/)
+ * to precache all assets.
+ * It uses the InjectManifest function from 'workbox-webpack-plugin' inside
+ * webpack/webpack.common.js
+ */
+workbox.precaching.precacheAndRoute(__precacheManifest)
+
