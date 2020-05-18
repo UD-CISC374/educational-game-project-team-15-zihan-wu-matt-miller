f507881bcc4ab6f97dcafe26649e7787731cba34
Matt <matt.miller041798@gmail.com>
2020-05-18 16:21:12 -0400

    Updates

---
+++ dev/precache-manifest.a5d992442b62b2019d080a344aed5b43.js
@@ -0,0 +1,410 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "369c0235a668e6e3ed40ed16319728d0",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/arrow-keys-full.png"
+  },
+  {
+    "revision": "e00b9f7c33fef8b9b343155cabdabc00",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/arrow-keys-single.png"
+  },
+  {
+    "revision": "8860206ce7c4c9e6bedec10a7bd7b61c",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/arrow_black.png"
+  },
+  {
+    "revision": "49a1af1a7f205d83066535b4a1f71fa1",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/arrow_white.png"
+  },
+  {
+    "revision": "521882646056092e2a90cee445c49d1f",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/back-down.png"
+  },
+  {
+    "revision": "4250a9fa43ecce767ca6829eee466745",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/back-up.png"
+  },
+  {
+    "revision": "213944f46da4fe1dd10b2b6c40f444f7",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/bkg.png"
+  },
+  {
+    "revision": "0f3ced6e78933083c5d459fb3e3239ec",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/bluegem.png"
+  },
+  {
+    "revision": "4e3bba8bd63519433341cf53fa9a6c9a",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/button-template.png"
+  },
+  {
+    "revision": "ada77b74fa425cbf2679a8e51c49f2fa",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/camocaper.json"
+  },
+  {
+    "revision": "e3ded070cc7059e41d2a48a52bfd993a",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/camocaper.tmx"
+  },
+  {
+    "revision": "596da5b37ee14242be281aabaa826181",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/camotiles.png"
+  },
+  {
+    "revision": "cd016ddf1954a11d06ddcd8f1ae6e616",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/camotiles_old.png"
+  },
+  {
+    "revision": "fba88d26c7328028e97d39f9ad947696",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/catchara.png"
+  },
+  {
+    "revision": "fd3a20fea9b2ce46e52559c4995a8aa8",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/citations.md"
+  },
+  {
+    "revision": "6757b9c8356c820347802d04419631e6",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/controls-3-dwn.png"
+  },
+  {
+    "revision": "a64714df2e0287e050a301fff539dd3e",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/controls-3-up.png"
+  },
+  {
+    "revision": "d8976d308346afdcd24a9de8c4d6b5fb",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/gem.png"
+  },
+  {
+    "revision": "53cefca2a0f75654486b180949686165",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/icons/favicon.ico"
+  },
+  {
+    "revision": "8ab30def0ad109a97117bd62db942d75",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/icons/icons-16.png"
+  },
+  {
+    "revision": "feee71db2bb3ba5d11b26658baf97783",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/icons/icons-192.png"
+  },
+  {
+    "revision": "cb71713be71d0b8820c7ea6af9c5ddc6",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/icons/icons-512.png"
+  },
+  {
+    "revision": "200ce541edfaa71be93a1041736d1fee",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/icons/icons8-game-controller-16.png"
+  },
+  {
+    "revision": "4cea6c8e0454de7a59c3c2acd5a3372a",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/info-formatted.png"
+  },
+  {
+    "revision": "e2211c4f1f95c441cc283dfd117c6492",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/info.png"
+  },
+  {
+    "revision": "94ecadd667fc2f6a0e56d54a0108a286",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/jail.png"
+  },
+  {
+    "revision": "fe2a93637af3b9c72b0069ef74f069c5",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/keys.png"
+  },
+  {
+    "revision": "2c7850f2bca7beec7e91e9c615774f09",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/level-1.json"
+  },
+  {
+    "revision": "e2388d20074ddd42e17e32836d5c8af1",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/level-2.json"
+  },
+  {
+    "revision": "08e98aef08f724ccb7784da614369ba8",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/level-3.json"
+  },
+  {
+    "revision": "516c29844f4683a2ad539d9d2a3344c4",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/menu-black.png"
+  },
+  {
+    "revision": "f27d102795026ff78c7247b46a57c242",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/menu-white.png"
+  },
+  {
+    "revision": "05001735150d9f7d214701ea2df32f9a",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/move-tutorial-black-1.png"
+  },
+  {
+    "revision": "bb37625dea896f7814a27841d6fa6df6",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/move-tutorial-black.png"
+  },
+  {
+    "revision": "6b38f81cf61c5bc1e76c32591ecd3e29",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/move-tutorial-white-1.png"
+  },
+  {
+    "revision": "511b965fcdc74918498051c39575f846",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/move-tutorial-white.png"
+  },
+  {
+    "revision": "a40993b9cc3d4941fd7d9b8362522b8a",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/no-black.png"
+  },
+  {
+    "revision": "71abf3a04cbcfe811892ea795e88cefc",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/no-white.png"
+  },
+  {
+    "revision": "e4266d1f808e8a241b4eaf2a05190006",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/play-2-black.png"
+  },
+  {
+    "revision": "fcb1418f37af597f01cd7df1aaa044a5",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/play-2-white.png"
+  },
+  {
+    "revision": "78d9e79bdf7caeadf38fb8a5a07b4111",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/play-2.png"
+  },
+  {
+    "revision": "3d64f149523e2f7b7a58d7532d359d68",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/play-3-black.png"
+  },
+  {
+    "revision": "26915c601dd62925b8dd60961df8e350",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/play-3-dwn.png"
+  },
+  {
+    "revision": "fbc44333ed4dfc08898b6c672bb25a0b",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/play-3-up.png"
+  },
+  {
+    "revision": "8077e70ea86f853b2e43c344ca85068d",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/play-3-white.png"
+  },
+  {
+    "revision": "0a550269321c170cdbfd68c98647fce5",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/play-3.png"
+  },
+  {
+    "revision": "17910e90e56f864e7366a3efed02145b",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/play-bttn-dwn.png"
+  },
+  {
+    "revision": "5d82c81ce22e5de22db4cfad860709fe",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/play-bttn-up.png"
+  },
+  {
+    "revision": "2d953857e67d55564ebb025addd9c1f7",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/sounds/alarm-1.ogg"
+  },
+  {
+    "revision": "51e8b976d90ccd56ef16ca54db12710f",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/sounds/bkg-1-cropped.ogg"
+  },
+  {
+    "revision": "e74330e1326a0e379ed3f69d6f035491",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/sounds/bttn-tick.ogg"
+  },
+  {
+    "revision": "f65099af915815953d5175dce65730a5",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/sounds/click-1.ogg"
+  },
+  {
+    "revision": "0cf325396f0f5cb8e4c7b22ac70f53cf",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/sounds/diamond-1.ogg"
+  },
+  {
+    "revision": "31e2e5d3ceff258aa7ca6a92fc71fd32",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/sounds/music/Alexander Zhelanov Betrayed.mp3"
+  },
+  {
+    "revision": "3379e2896872d382557f49ada2a2a1df",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/sounds/music/Cats And Spies.wav"
+  },
+  {
+    "revision": "919b5495617dce0d4c45af22f0ef14b1",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/sounds/music/espionage.ogg"
+  },
+  {
+    "revision": "17611504d79197ee539bb22975ef2f05",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/sounds/music/matthewpablo/Deliciously Sour.mp3"
+  },
+  {
+    "revision": "c5d33f1c46153b07b11d696571c632fd",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/sounds/music/matthewpablo/Evasion.mp3"
+  },
+  {
+    "revision": "ec73fa85a4567de80a78bd5974627321",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/sounds/music/matthewpablo/Trouble Makers (Loopable).wav"
+  },
+  {
+    "revision": "02c736c82971071d5cbbd36b1e47d8b2",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/sounds/music/matthewpablo/Trouble Makers.mp3"
+  },
+  {
+    "revision": "6eaa70286bf48c730149e5d3e6620067",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/sounds/music/matthewpablo/Wasteland Overdrive/Wasteland Overdrive (No Choirs).mp3"
+  },
+  {
+    "revision": "43fb4e16097e17331153ec1bdcadbbc0",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/sounds/music/matthewpablo/Wasteland Overdrive/Wasteland Overdrive Looped (No Choir).wav"
+  },
+  {
+    "revision": "801081aee79bdbe34ca20ce8c32c77f6",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/sounds/music/matthewpablo/Wasteland Overdrive/Wasteland Overdrive Looped.wav"
+  },
+  {
+    "revision": "95fb82d44de624461d5f26852b2930a3",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/sounds/music/matthewpablo/Wasteland Overdrive/Wasteland Overdrive.mp3"
+  },
+  {
+    "revision": "4cecb5edf46573b06bd5184bfff72097",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/sounds/music/redeemed_victory.ogg"
+  },
+  {
+    "revision": "373505b6fcbfcd53334047063731c1df",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/sounds/music/redeemed_victory.wav"
+  },
+  {
+    "revision": "54b38cce52d7d4ec55a8e637c4bb260a",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/sounds/pluck-success-1.ogg"
+  },
+  {
+    "revision": "db91e35213bac8a1f0e3b324e650a4b7",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/sounds/reward-1.ogg"
+  },
+  {
+    "revision": "5ad8b591225d2c139567ab2ec6a95aec",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/sounds/siren-1-short.ogg"
+  },
+  {
+    "revision": "55c4704f4780b260556202c10936b7a0",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/sounds/sirens/240207__thedoctorgoat__police-siren-final.wav"
+  },
+  {
+    "revision": "7a7e127da5f5d7cff30134d5c3725b0f",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/sounds/sirens/52906__vlammenos__police-siren.wav"
+  },
+  {
+    "revision": "060ea02bf63cb72827e2ee83daf6225e",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/sounds/sirens/Alarm.wav"
+  },
+  {
+    "revision": "20e90fa2343c53495d21c177b7a850d8",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/sounds/sirens/tx-alert/alert-critical.wav"
+  },
+  {
+    "revision": "afebcf3acbf3119463c841e62cefda73",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/sounds/sirens/tx-alert/alert-normal.wav"
+  },
+  {
+    "revision": "ab8f713c33447bb5d1b6c5e6a8dcef0a",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/sounds/sirens/tx-alert/metadata.txt"
+  },
+  {
+    "revision": "40817f89b80f8232d7b4e3d02f70f118",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/sounds/sound_citations.txt"
+  },
+  {
+    "revision": "bd857cf09c52954b0ed7fb222ac4eb2b",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/sounds/soundeff/133008__cosmicd__annulet-of-absorption.wav"
+  },
+  {
+    "revision": "1bc27d1be432ba311c34d16cf99c5595",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/sounds/soundeff/266100__marregheriti__level-up.wav"
+  },
+  {
+    "revision": "e485ad0c5821720fc91a6578b69e6e3d",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/sounds/soundeff/320654__rhodesmas__level-up-02.wav"
+  },
+  {
+    "revision": "abb524a05f744ea620ddd772d676737c",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/sounds/soundeff/371205__mouse85224__level-failed.aiff"
+  },
+  {
+    "revision": "ab5628dc06012ab637b6f8d5f4e9e321",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/sounds/soundeff/415209__inspectorj__cat-screaming-a.wav"
+  },
+  {
+    "revision": "f939b8011ab26fd0bfae693d901ff51d",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/sounds/soundeff/Cloud Click.wav"
+  },
+  {
+    "revision": "8f8a8b98c655870f6b0502069cc630bc",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/sounds/soundeff/Menu Selection Click.wav"
+  },
+  {
+    "revision": "921d684066f9675d4a6ae5af67341cc9",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/sounds/soundeff/Picked Coin Echo 2.wav"
+  },
+  {
+    "revision": "d20ed437491b7092f24a718d44a74507",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/sounds/soundeff/Toom Click.wav"
+  },
+  {
+    "revision": "c64838f0508fd918b5c84852251b52f1",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/sounds/soundeff/timbre__metal-clang.flac"
+  },
+  {
+    "revision": "ce2d1d1e397473c298a44c5bab84200f",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/sounds/soundeff/timbre__metal-clang.ogg"
+  },
+  {
+    "revision": "152f3e5215cdffbaf40a2026e816b6da",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/sounds/wrong-1.ogg"
+  },
+  {
+    "revision": "885d705734700dbe57579e0135c46747",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/start-bkg-4k.jpg"
+  },
+  {
+    "revision": "7c9aa037a67baff060c99e3733da3b49",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/start-bkg-reg.jpg"
+  },
+  {
+    "revision": "00e552c176c7beeb813933fc9f5b3d3e",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/start-bkg-reg.png"
+  },
+  {
+    "revision": "291ad4b402061b8d26947a95528b9151",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/title.png"
+  },
+  {
+    "revision": "2f664528ea817529fe8fd6f320419725",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/tutorial/pkmn.png"
+  },
+  {
+    "revision": "f3a8b37f3ded924719d6a3acdb5d1a5e",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/tutorial/tiledemo.json"
+  },
+  {
+    "revision": "c702cf698ec2177ac192bbd4007c13a2",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/yes-black.png"
+  },
+  {
+    "revision": "e287b69dec41f066a9dcf86e8c7da458",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devassets/yes-white.png"
+  },
+  {
+    "revision": "53cefca2a0f75654486b180949686165",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devfavicon.ico"
+  },
+  {
+    "revision": "64cd0de75e2319eeb0d0",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devgen.main.86420ddec4dcd19e1686.bundle.js"
+  },
+  {
+    "revision": "2028022e166679461cd3",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devgen.vendors.b456385bc0593d898ea2.bundle.js"
+  },
+  {
+    "revision": "dde3a6f6a779009d42a6529e7f5a74fd",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devindex.html"
+  },
+  {
+    "revision": "e3a72ed08fea663494fd70b7b7e9beff",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devmanifest.json"
+  },
+  {
+    "revision": "a8029749c7edd38204cf67287cb14139",
+    "url": "/educational-game-project-team-15-zihan-wu-matt-miller/devsw.js"
+  }
+]);
--- dev/sw.js
+++ dev/sw.js
@@ -1,4 +1,4 @@
-importScripts("/educational-game-project-team-15-zihan-wu-matt-miller/devprecache-manifest.94e0d511c45f571e1b4bac77521c9e15.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/educational-game-project-team-15-zihan-wu-matt-miller/devprecache-manifest.a5d992442b62b2019d080a344aed5b43.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 /**
  * You should only modify this, if you know what you are doing.
